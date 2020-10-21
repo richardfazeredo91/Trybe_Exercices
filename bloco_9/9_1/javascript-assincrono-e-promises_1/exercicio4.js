3890079376abeab5bdfaf69e82367eaab4762df7
richardfazeredo <richardcompletetour@gmail.com>
2020-10-21 19:18:26 -0300

    Calling a callback function inside a hof asynchronous function that consoles a message concatenated with its calling

---
+++ bloco_9/9_1/javascript-assincrono-e-promises_1/exercicio4.js
@@ -0,0 +1,17 @@
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+// crie a função sendMarsTemperature abaixo
+
+const sendMarsTemperature = (callback) => {
+  return setTimeout(() => {
+    console.log(`Mars temperature is: ${callback()} degree Celsius`)
+  }), messageDelay;
+}
+
+
+sendMarsTemperature(getMarsTemperature); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
