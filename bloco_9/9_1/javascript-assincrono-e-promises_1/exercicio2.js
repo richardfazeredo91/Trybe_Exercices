4d97eb3086e1705be7c1b79409ec9f9cd46ee559
richardfazeredo <richardcompletetour@gmail.com>
2020-10-21 18:49:02 -0300

    Calling synchronous and asynchronous functions

---
+++ bloco_9/9_1/javascript-assincrono-e-promises_1/exercicio2.js
@@ -0,0 +1,35 @@
+const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+  `${name} is ${value} ${measurementUnit} apart from the Sun`;
+
+const mars = {
+  name: "Mars",
+  distanceFromSun: {
+    value: 227900000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const venus = {
+  name: "Venus",
+  distanceFromSun: {
+    value: 108200000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const jupiter = {
+  name: "Jupiter",
+  distanceFromSun: {
+    value: 778500000,
+    measurementUnit: "kilometers",
+  },
+};
+
+console.log(planetDistanceFromSun(mars)); // A
+setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // B
+setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C
+
+/*Serão executados de forma assíncrona durante a chamada das funções,
+a começar pela função síncrona que recebe o parâmetro mars, em seguida
+a função assíncrona que recebe o parâmetro jupiter, após passados 2 segundos,
+por último a segunda função com o parâmetro vênus, após passados 3 segundos.*/ 
