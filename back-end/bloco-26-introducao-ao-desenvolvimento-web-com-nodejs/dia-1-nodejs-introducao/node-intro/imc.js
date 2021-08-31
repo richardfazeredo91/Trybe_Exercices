19cd163244ebe9c48a033e27ea1d19639189f71a
richardfazeredo <richardcompletetour@gmail.com>
2021-08-31 15:07:46 -0300

    Adicionada funçao que retorna categoria de uma pessoa de acordo com seu IMC

--- back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-introducao/node-intro/imc.js
+++ back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-introducao/node-intro/imc.js
@@ -5,4 +5,20 @@ const altura = readline.questionFloat('Qual a sua altura? ');
 
 const imc = peso / Math.pow(altura, 2);
 
-console.log(imc);
+const categoriaImc = (imcResult) => {
+  if (imcResult < 18.5) {
+    return 'Abaixo do peso (magreza)';
+  } else if (imcResult >= 18.5 && imcResult < 25) {
+    return 'Peso normal';
+  } else if (imcResult >= 25 && imcResult < 30) {
+    return 'Acima do peso (Sobrepeso)';
+  } else if (imcResult >= 30 && imcResult < 35) {
+    return 'Obesidade grau I';
+  } else if (imcResult >= 35 && imcResult < 40) {
+    return 'Obesidade grau II';
+  } else {
+    return 'Obesidade graus III e IV ';
+  }
+}
+
+console.log (`IMC: ${imc} | Situação: ${categoriaImc(imc)}`);
