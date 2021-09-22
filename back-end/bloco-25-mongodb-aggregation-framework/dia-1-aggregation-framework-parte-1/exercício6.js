c997c217deecc67cac7fe966cc939c5771fe8d99
richardfazeredo <richardcompletetour@gmail.com>
2021-09-22 00:39:41 -0300

    Exercicio 6

---
+++ back-end/bloco-25-mongodb-aggregation-framework/dia-1-aggregation-framework-parte-1/exercício6.js
@@ -0,0 +1,18 @@
+use('erp');
+
+db.clientes.aggregate(
+  [
+    {
+      $group: {
+        _id: { sexo: "$sexo", uf: "$endereco.uf" },
+        total: { $sum: 1 },
+      }
+    },
+    {
+      $project: {
+        _id: "$_id.sexo",
+        total: '$total',
+      }
+    }
+  ],
+);
