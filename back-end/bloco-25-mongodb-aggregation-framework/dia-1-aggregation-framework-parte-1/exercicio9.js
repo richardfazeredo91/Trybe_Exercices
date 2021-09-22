1581ccca9e841619bffabd5d3c85dc3ed3e77fdf
richardfazeredo <richardcompletetour@gmail.com>
2021-09-22 02:54:51 -0300

    Exercicio 9

---
+++ back-end/bloco-25-mongodb-aggregation-framework/dia-1-aggregation-framework-parte-1/exercicio9.js
@@ -0,0 +1,22 @@
+use('erp');
+
+db.vendas.aggregate(
+  [
+    {
+      $group: {
+        _id: "$clienteId",
+        qtdCompras: { $sum: 1 },
+      },
+    },
+    {
+      $match: {
+        qtdCompras: {
+          $gt: 5,
+        },
+      },
+    },
+    {
+      $count: "clientes",
+    },
+  ],
+);
