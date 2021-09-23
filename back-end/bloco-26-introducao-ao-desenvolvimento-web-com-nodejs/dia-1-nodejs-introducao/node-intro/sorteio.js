153a9709c48acc93822e2f45a1aef9d9703b2652
richardfazeredo <richardcompletetour@gmail.com>
2021-09-23 16:00:35 -0300

    Corrigindo script de advinhação numérica, colocando função de geração randomica para dentro do laço de repetição

--- back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-introducao/node-intro/sorteio.js
+++ back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-introducao/node-intro/sorteio.js
@@ -3,21 +3,24 @@ const readline = require('readline-sync');
 const min = Math.ceil(0);
 const max = Math.floor(10);
 
-const drawnNumber = Math.floor(Math.random() * (max - min + 1)) + min;
 
 const startGame = (replay = 'no') => {
-
+  
   do {
+    const drawnNumber = Math.floor(Math.random() * (max - min + 1)) + min;
     const choosenNumber = readline.questionInt('Insira um número entre 0 e 10: ');
     
     if (choosenNumber === drawnNumber) {
       console.log('Parabéns, número correto!');
     } else {
       console.log(`Opa, não foi dessa vez. O número era ${drawnNumber}`);
-    }
+    };
 
-    replay = readline.question('Deseja jogar novamente? Se sim, digite "yes" ');
+    replay = readline
+      .question(
+        'Deseja jogar novamente? Se sim, digite "yes" ou digite "no" para encerrar. ',
+      );
   } while (replay === 'yes');
-}
+};
 
 startGame();
