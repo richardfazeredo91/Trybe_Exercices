569f61a47fa6a4f2f9f6e8de524847092dd277ba
richardfazeredo <richardcompletetour@gmail.com>
2021-09-24 03:31:35 -0300

    Exercício bônus, criado script que gera n números da sequência fibonacci

---
+++ back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-introducao/node-intro/fibonacci.js
@@ -0,0 +1,31 @@
+const readline = require('readline-sync');
+
+function getFibonacci() {
+  const number = readline
+    .questionInt(
+      'Insira uma quantidade de números que deseja obter da sequencia fibinacci: ',
+    );
+  let sequence = [1, 1];
+
+  if (number <= 0) {
+    console.log('Número inválido. Por favor, insira um número maior que 0.');
+    return;
+  }
+
+  while(sequence.length < number) {
+    const calcNext = () => {
+      const twoLast = sequence.slice(-2);
+      const sum = twoLast[0] + twoLast[1];
+      return sum;
+    };
+
+    sequence.push(calcNext());
+  };
+
+  const formatedSequence = sequence.map(num => ` ${ num }`);
+
+  console.log(`Sequencia fibonacci de ${ number } números:${ formatedSequence }`);
+  return sequence;
+};
+
+getFibonacci();
