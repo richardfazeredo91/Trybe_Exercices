c38378658ea6b4b6c5af996efdb110f1589994d8
richardfazeredo <richardcompletetour@gmail.com>
2021-09-24 00:07:11 -0300

    Modificando mensagem de replay do exercício 7

--- back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-introducao/node-intro/sorteio.js
+++ back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-introducao/node-intro/sorteio.js
@@ -3,9 +3,7 @@ const readline = require('readline-sync');
 const min = Math.ceil(0);
 const max = Math.floor(10);
 
-
 const startGame = (replay = 'no') => {
-  
   do {
     const drawnNumber = Math.floor(Math.random() * (max - min + 1)) + min;
     const choosenNumber = readline.questionInt('Insira um número entre 0 e 10: ');
