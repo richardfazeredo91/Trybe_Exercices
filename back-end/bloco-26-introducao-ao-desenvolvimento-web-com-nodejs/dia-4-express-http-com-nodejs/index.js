245366c110a4192b6283f3ab9768a5143d9ed879
richardfazeredo <richardcompletetour@gmail.com>
2021-10-01 16:43:22 -0300

    Criando nova rota com método post que recebe o nome do usuário e retorna mensagem "Hello, nome_do_usuário"

--- back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
+++ back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/ping', (req, res) => res.json({ message: 'pong' }));
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  res.status(201).json({ "message": `Hello, ${ name }!`})
+})
 
 app.listen('3000', () => console.log('Rodando na porta 3000!'));
