8f6ddf7d75dda9ac4718ecded253e07f31dd3f03
richardfazeredo <richardcompletetour@gmail.com>
2021-10-01 17:16:45 -0300

    Criada nova rota com método put, que retorna mensagem ao usuário com as informações que são passadas na rota.

--- back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
+++ back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { restart } = require('nodemon');
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,6 +14,11 @@ app.post('/greetings', (req, res) => {
 
   if (age > 17) { res.status(200).json({ "message": `Hello, ${ name }!` })};
   res.status(401).json({ "message": "Unauthorized" });
-})
+});
+app.put('/users/:name/:age', (req, res) => {
+  const { name, age } = req.params;
+  res.status(200)
+    .json({ "message": `Seu nome é ${ name } e você tem ${ age } anos de idade` })
+});
 
 app.listen('3000', () => console.log('Rodando na porta 3000!'));
