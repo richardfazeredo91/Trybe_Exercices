70450ec09b5fd8e26a40b17d3419b338d4bc81cd
richardfazeredo <richardcompletetour@gmail.com>
2021-10-01 16:59:04 -0300

    Criada rota que retorna mensagem de saudações para o usuário, caso ele for menor de 18 anos, retorna mensagem de não autorizado

--- back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
+++ back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { restart } = require('nodemon');
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,6 +9,12 @@ app.get('/ping', (req, res) => res.json({ message: 'pong' }));
 app.post('/hello', (req, res) => {
   const { name } = req.body;
   res.status(201).json({ "message": `Hello, ${ name }!`})
+});
+app.post('/greetings', (req, res) => {
+  const { name, age } = req.body;
+
+  if (age > 17) { res.status(200).json({ "message": `Hello, ${ name }!` })};
+  res.status(401).json({ "message": "Unauthorized" });
 })
 
 app.listen('3000', () => console.log('Rodando na porta 3000!'));
