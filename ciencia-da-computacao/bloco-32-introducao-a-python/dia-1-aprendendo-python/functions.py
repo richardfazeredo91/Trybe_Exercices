aef2e5d27e4d98273e163889030b816f31be5731
richardfazeredo91 <richardbrprogrammer@gmail.com>
2022-01-17 19:19:22 -0300

    Implementada função que retorna o maior nome de uma determinada lista

--- ciencia-da-computacao/bloco-32-introducao-a-python/dia-1-aprendendo-python/functions.py
+++ ciencia-da-computacao/bloco-32-introducao-a-python/dia-1-aprendendo-python/functions.py
@@ -17,3 +17,9 @@ def asterisks_square_generator(length):
         for i in range(length):
             print (length * '*')
 
+def get_biggest_name(names):
+    biggest_name = ''
+    for name in names:
+        if len(name) > len(biggest_name):
+            biggest_name = name
+    return biggest_name
