1f96e3b73d43794719b5808bbfb0dd9584660560
richardfazeredo91 <richardbrprogrammer@gmail.com>
2022-01-17 19:00:51 -0300

    Implementada função que obtem média aritmética de uma lista numérica

--- ciencia-da-computacao/bloco-32-introducao-a-python/dia-1-aprendendo-python/functions.py
+++ ciencia-da-computacao/bloco-32-introducao-a-python/dia-1-aprendendo-python/functions.py
@@ -5,3 +5,13 @@ def biggest_number(num1, num2):
         return num2
 
 print (biggest_number(9, 6))
+
+def get_media(numbers):
+    total = 0
+    for number in numbers:
+        total += number
+    return round(total / len(numbers), 2)
+
+number_list = [1, 3, 6, 12, 15, 19]
+
+print (get_media(number_list))
